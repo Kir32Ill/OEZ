8e3d4c8e064f7ced25e52b0bc00264dbd92f9bfe
Kirill Golovanov <cgolowanov@yandex.ru>
2024-12-09 02:42:49 +0300

    Second demo version of project

--- sript.js
+++ sript.js
@@ -1,27 +1,58 @@
 function getMaterial(){
     const diametrOfDetail = document.getElementById("Diametr");
     const highOfDetail = document.getElementById("High");
+    const variantOfDetail=myForm2.variant.value; 
     const submitBtn=document.getElementById("submit");
     const resultElement = document.getElementById('result');
     const material=document.getElementById('material');
+    const price = document.getElementById('Price');
+    const radioButton=document.myForm2.variant;
+    let priceOfKg=price.value;
     let getMat = material.value;
-    let price;
-    if(getMat=="Сталь")
-        price= 10;
-    else if(getMat=="Медь")
-        price= 7;
-    else if(getMat=="Титан")
-        price= 15;
-    function calculateVolume(diametr, high, pr){
-        let volume=Math.PI *(diametr/2)**2 * high*pr;
-        return volume;
+    let di=+diametrOfDetail.value;
+    let hi=+highOfDetail.value;
+    console.log(getMat);
+    console.log(variantOfDetail);
+    console.log(priceOfKg);
+    let choosedVariant=1;
+    if(getMat==="Болт"){
+        if (variantOfDetail==="Круг"){
+            choosedVariant=calculateVolumeCircle(di/1000,hi/1000,priceOfKg);
+        }else{
+            choosedVariant=calculateShestBolt(di,hi,priceOfKg);
+        }
+    }else if(getMat=="Гайка"){
+        if (variantOfDetail=="Круг"){
+            choosedVariant=calculateVolumeCircle(di/1000,hi/1000,priceOfKg);
+        }else{
+            choosedVariant=calculateShestGaika(di,priceOfKg);
+        }
+    }else{
+        choosedVariant=calculateVolumeCircle(di/1000,hi/1000,priceOfKg);
     }
     submitBtn.onclick = function () {
-        const result=calculateVolume(diametrOfDetail.value, highOfDetail.value,price);
-        resultElement.textContent= result.toFixed(3);
+        const result=choosedVariant.toFixed(3);
+        let fullresult=variantOfDetail +": "+ String(result) + " руб.";
+        resultElement.textContent= fullresult;
+        radioButton.forEach(radio=>{
+            radio.checked=false;
+        })
     }
 }
-
-
+function calculateVolumeCircle(diametr, high, price){
+    
+    let volume=Math.PI *(diametr/2)**2 * high*2.45*price;
+    console.log(Math.PI *(diametr/2)**2 * high*2.45*price);
+    return volume;
+}
+function calculateShestBolt(diametr, high,price){
+    let volume=(0.8*diametr+high)/1000*2.45*price;
+    return volume;
+}
+function calculateShestGaika(diametr,price){
+    let volume=(0.8*diametr)/1000*2.45*price;
+    console.log(0.8*diametr)/1000;
+    return volume;
+}
 
 
